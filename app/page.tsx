83e20e8f56d9c0783069ad9cefa605bfabf28586
NithinKonda <nithinkonda142@gmail.com>
2024-09-22 20:39:24 +0530

    Change route

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
       <div>
         <h1 className="text-center">TOWER-GAME</h1>
-        <Link href="/lobby">Get in Lobby</Link>
+        <Link href="/game">Game</Link>
       </div>
   );
 }
